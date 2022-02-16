27aea41cf431d926179a5f6984895f55bca60313
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-02-16 18:06:47 +0300

    Add github-repos component to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { GithubReposComponent } from './components/github-repos/github-repos.component';
 
 const routes: Routes = [
-  { path: 'profile', component: GithubProfileComponent }
+  { path: 'profile', component: GithubProfileComponent },
+  { path: 'repos', component: GithubReposComponent }
 ];
 
 @NgModule({
