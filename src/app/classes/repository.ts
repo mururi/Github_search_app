7259b6572d73bb25596627d866e3a7e98be72eb6
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-02-16 14:41:37 +0300

    Add details to user and repository classes

--- src/app/classes/repository.ts
+++ src/app/classes/repository.ts
@@ -1,2 +1,3 @@
 export class Repository {
+  constructor(public name: string, public html_url: string, public description: string, public created_at: Date) { }
 }
--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,2 +1,3 @@
 export class User {
+  constructor(public login: string, public avatar_url: string, public repos_url: string, public name: string, public bio: string, public public_repos: number, public followers: number, public following: number, public created_at: Date){ }
 }
